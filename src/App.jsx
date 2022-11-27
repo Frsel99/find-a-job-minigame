758c758eef38dda279e3b78d04f80bb2831a2cbd
Franco Leyes <84664199+Frsel99@users.noreply.github.com>
2022-11-27 08:36:06 -0300

    Added new components & testings

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import "./App.css";
-import React from "react";
-
-function App() {
-  return <div>Probando el deploy</div>;
-}
-
-export default App;
---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import UpgradeBar from "../UpgradeBar/UpgradeBar";
+
+function App() {
+  return (
+    <div>
+      <UpgradeBar />
+    </div>
+  );
+}
+
+export default App;
